6e952d5af649066b4f8c6bed1832b9302c4078c1
Oleksii Kovalchuk <oleksiij.ko@gmail.com>
2020-09-13 18:08:58 +0300

    Add router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,14 @@
 import * as React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import { Product } from "./";
 
 import "../styles/index.scss";
 
-const App = () => <></>;
+const App = () => (
+  <Switch>
+    <Route path="/products/:name" component={Product} />
+  </Switch>
+);
 
 export default App;
